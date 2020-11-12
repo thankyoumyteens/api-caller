2715d17c398416e1a0263a2575a4fc4a38262e38
zhaoshengzhi <zhaosz@sobot.com>
2020-11-12 11:45:09 +0800

    a

---
+++ HttpUtil.py
@@ -0,0 +1,37 @@
+import requests
+
+
+class HttpUtil:
+
+    def __init__(self):
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/86.0.4240.193 Safari/537.36'
+        }
+
+    def set_headers(self, headers):
+        self.headers = headers
+
+    def set_cookies(self, cookies):
+        # k=v;k=v;k=v
+        self.headers['Cookie'] = cookies
+
+    def clear_cookies(self):
+        if 'Cookie' in self.headers:
+            del self.headers['Cookie']
+
+    def set_json_request(self):
+        self.headers['Content-Type'] = 'application/json'
+
+    def set_bearer_auth_token(self, key, token):
+        self.headers[key] = 'bearer {}'.format(token)
+
+    def post_json(self, url, params):
+        self.set_json_request()
+        r = requests.post(url, json=params, headers=self.headers)
+        try:
+            text = r.json()
+        except ValueError:
+            text = r.text
+        return text
