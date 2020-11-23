f6c3292d3ed09b806a7a039470803d9dea95e3a7
zhaoshengzhi <iloveyesterday@outlook.com>
2020-11-23 13:08:12 +0800

    'p'

--- HttpUtil.py
+++ HttpUtil.py
@@ -1,6 +1,12 @@
 import requests
 
 
+class HttpResponse:
+    def __init__(self, body, headers):
+        self.body = body
+        self.headers = headers
+
+
 class HttpUtil:
 
     def __init__(self):
@@ -9,6 +15,9 @@ class HttpUtil:
                           'AppleWebKit/537.36 (KHTML, like Gecko) '
                           'Chrome/86.0.4240.193 Safari/537.36'
         }
+
+    def set_header(self, key, value):
+        self.headers[key] = value
 
     def set_headers(self, headers):
         self.headers = headers
@@ -21,17 +30,17 @@ class HttpUtil:
         if 'Cookie' in self.headers:
             del self.headers['Cookie']
 
-    def set_json_request(self):
+    def set_json_request_header(self):
         self.headers['Content-Type'] = 'application/json'
 
     def set_bearer_auth_token(self, key, token):
         self.headers[key] = 'bearer {}'.format(token)
 
     def post_json(self, url, params):
-        self.set_json_request()
+        self.set_json_request_header()
         r = requests.post(url, json=params, headers=self.headers)
         try:
             text = r.json()
         except ValueError:
             text = r.text
-        return text
+        return HttpResponse(text, r.headers)
